f9190ea582162edc4ec082b34d7289d46753eb44
Jian Yeung <yeungjian@gmail.com>
2019-08-23 10:14:56 -0700

    added db helpers and api code for getting homestay and reservations

--- server/server.js
+++ server/server.js
@@ -2,8 +2,30 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
+const db = require('../dbHelper/serverDBHelper')
 
 app.use(express.static('public'));
+
+//FORMAT: /homestay/?id=[homestay_id]
+//SAMPLE: http://localhost:3000/homestay/?id=1
+app.get('/homestay', (req, res) => {
+  let homestayId = req.query.id;
+  db.getHomestayById(homestayId)
+  .then(data => {
+    res.json(data);
+  })
+})
+
+//FORMAT: reservations/?id=[homestay_id]&month=[month as int]
+//SAMPLE: http://localhost:3000/reservations/?id=2&month=10
+app.get('/reservations', (req, res) => {
+  let homestayId = req.query.id;
+  let month = req.query.month;
+  db.getReservationsByMonth(homestayId, month)
+  .then(data => {
+    res.json(data);
+  })
+})
 
 app.listen(port, () => {
   console.log('listening on port:', port);
