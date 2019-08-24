c99066f35c93f83cf4339fe8474cdefa46def546
Jian Yeung <yeungjian@gmail.com>
2019-08-23 20:43:40 -0700

    fixed bug in calendar rendering

--- client/app.jsx
+++ client/app.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import BasicCalendar from './Components/BasicCalendar';
 
-ReactDOM.render(<BasicCalendar year={2019} month={7} homestayId={1} type="checkout" />, document.getElementById('app'));
+ReactDOM.render(<BasicCalendar year={2019} month={7} homestayId={1} type="checkin" />, document.getElementById('app'));
