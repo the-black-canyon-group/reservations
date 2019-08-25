df3c790291a0d2e27d6b9bcfcc7c4d4fea2d1be1
Jian Yeung <yeungjian@gmail.com>
2019-08-24 19:08:53 -0700

    adding border to calendar

--- client/app.jsx
+++ client/app.jsx
@@ -4,5 +4,5 @@ import ReactDOM from 'react-dom';
 import BasicCalendar from './Components/BasicCalendar';
 import ReservationBox from './Components/ReservationBox';
 
-// ReactDOM.render(<BasicCalendar year={2019} month={7} homestayId={1} type="checkin" />, document.getElementById('app'));
-ReactDOM.render(<ReservationBox homestayId={2} />, document.getElementById('app'));
+ReactDOM.render(<ReservationBox homestayId={2} />, document.getElementById('app1'));
+ReactDOM.render(<BasicCalendar year={2019} month={7} homestayId={1} type="checkin" />, document.getElementById('app2'));
